b61ab59065c4efcbc318a16185e47f892642ee91
Jérémie Lesage <jeremie.lesage@jeci.fr>
2016-03-10 10:21:24 +0100

    correcting sum calculation

--- src/main/resources/sql/hsqldb/localdb/update_dir_sum_size.sql
+++ src/main/resources/sql/hsqldb/localdb/update_dir_sum_size.sql
@@ -1,8 +1,8 @@
--- Upadte DIR_SUM_SIZE for select node
+-- Update DIR_SUM_SIZE for select node
 update STATS_DIR_LOCAL_SIZE a
 set a.DIR_SUM_SIZE = (
-	select sum(b.DIR_LOCAL_SIZE)
+	select sum( isnull( b.DIR_LOCAL_SIZE, 0 ) ) + sum( isnull( b.DIR_SUM_SIZE, 0 ) )
 	from STATS_DIR_LOCAL_SIZE b
 	where b.PARENT_NODE_ID = a.NODE_ID
 )
-where a.NODE_ID in ( :ids )
+where a.NODE_ID = :ids
