a392b3bad9abeaf13a28dfcef140c6d3da4dfe8f
Jérémie Lesage <jeremie.lesage@jeci.fr>
2016-03-31 14:51:48 +0200

    namespace id may vary

--- src/main/resources/sql/mysql/alfresco41/select_node_label.sql
+++ src/main/resources/sql/mysql/alfresco41/select_node_label.sql
@@ -1,6 +1,10 @@
--- NS_ID 1 = sys:
--- NS_ID 6 = cm:
 select string_value
 from alf_node_properties
 where node_id = ?
-and qname_id = (select id from alf_qname where ns_id=6 and local_name = 'name')
+and qname_id = (
+	SELECT alf_qname.id 
+	FROM alf_qname
+	JOIN alf_namespace on alf_namespace.id = alf_qname.NS_ID
+	WHERE  local_name = 'name'
+	AND uri = 'http://www.alfresco.org/model/content/1.0'
+)
--- src/main/resources/sql/mysql/alfresco41/select_node_noderef.sql
+++ src/main/resources/sql/mysql/alfresco41/select_node_noderef.sql
@@ -1,5 +1,3 @@
--- NS_ID 1 = sys:
--- NS_ID 6 = cm:
 select  protocol, identifier, alf_node.uuid
 from alf_node
 join alf_store on alf_store.id = alf_node.store_id
